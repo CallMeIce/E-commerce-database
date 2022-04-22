c9434657d12c5d8a0240c7e179729ee78df9465c
Isaak Morales <ice.isaak97@gmail.com>
2022-04-21 21:14:22 -0400

    starting on the columns and figuring out why category is giving an error

--- models/Category.js
+++ models/Category.js
@@ -2,19 +2,84 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
-  },
-  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    {
     sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'category',
+  }});
 
 module.exports = Category;
+
+
+// Category
+
+
+// id
+
+
+// Integer.
+
+
+// Doesn't allow null values.
+
+
+// Set as primary key.
+
+
+// Uses auto increment.
+
+
+
+
+// category_name
+
+
+// String.
+
+
+// Doesn't allow null values.
+
+
+
+// Driver.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     address: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'driver',
+//   }
+// );
