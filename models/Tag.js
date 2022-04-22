dd759087fd124f105a597bf58a884532ef2f65c3
Isaak Morales <ice.isaak97@gmail.com>
2022-04-21 22:18:35 -0400

    Finished tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,15 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
@@ -18,8 +27,6 @@ Tag.init(
 );
 
 module.exports = Tag;
-
-
 
 
 // Tag
